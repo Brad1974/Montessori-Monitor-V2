0a617f938baa34ab12bdebf5e858b1fe780391a3
Brad1974 <danieltbrad@gmail.com>
2016-06-09 15:48:16 -0700

    created links from daily report json objects

--- app/assets/javascripts/daily_reports_ajax.js
+++ app/assets/javascripts/daily_reports_ajax.js
@@ -2,15 +2,14 @@ $(document).ready(function(){
 
   $('a.reports-link').on('click', function(e){
     e.preventDefault();
+    var link = $(this).attr('href')
     $.get($('a.reports-link').attr('href'), function(data){
       var reportList = ""
       data.forEach(function(details) {
-
-        var date = details.date;
-        var dailyReport = "<li>" + date + "</li>"
-        reportList += dailyReport
+        var dailyReport = "<li><a href= '/children/" + details.child_id + "/daily_reports/" + details.id + "'>" + details.date + "</a></li> ";
+        reportList += dailyReport;
       })
-        $('.report-area').html(reportList);
+    $('.report-area').html(reportList);
 
 
     })
@@ -18,5 +17,8 @@ $(document).ready(function(){
 
 
 
-
 })
+
+
+// original link i want to copy:
+// <a href="/children/1/daily_reports/1">Jun 01, 2016</a>
