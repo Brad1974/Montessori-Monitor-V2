f308af526c972bc75cbfe80c682ed08fd81e4d8c
Brad1974 <danieltbrad@gmail.com>
2016-06-09 14:01:49 -0700

    tweaked ajax request for daily report

--- app/assets/javascripts/daily_reports_ajax.js
+++ app/assets/javascripts/daily_reports_ajax.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
   $('a.reports-link').on('click', function(e){
     e.preventDefault();
     $.get($('a.reports-link').attr('href'), function(data){
+      var reportList = ""
       data.forEach(function(details) {
-        debugger;
+
         var date = details.date;
         var dailyReport = "<li>" + date + "</li>"
-        $('.report-area').append(dailyReport);
+        reportList += dailyReport
       })
+        $('.report-area').html(reportList);
 
 
     })
